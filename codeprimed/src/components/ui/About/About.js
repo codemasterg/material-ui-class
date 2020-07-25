2885f63031ac1b2071ce03ff836443e8060ca6c4
Greg Totsline <jtotsline@gmail.com>
2020-07-25 13:52:37 -0400

    added transition affect to About component.

--- codeprimed/src/components/ui/About/About.js
+++ codeprimed/src/components/ui/About/About.js
@@ -1,11 +1,11 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 
 import { useTheme } from '@material-ui/core/styles'
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import Grid from '@material-ui/core/Grid';
-import Tooltip from '@material-ui/core/Tooltip';
+import Grow from '@material-ui/core/Slide';
 import Typography from '@material-ui/core/Typography';
 import tileData from './tileData';
 
@@ -35,17 +35,19 @@ const About = (props) => {
     return (
         <>
             <Grid container alignContent="center" justify="center">
-            <Typography ref={picRef} style={ {...theme.typography.subtitle1, marginLeft: "1em"} }>Click or hover to learn about me.</Typography>
+                <Typography ref={picRef} style={{ ...theme.typography.subtitle1, marginLeft: "1em" }}>Click or hover to learn about me.</Typography>
             </Grid>
-            <div className={classes.root}>
-                <GridList cellHeight={160} className={classes.gridList} cols={3}>
-                    {tileData.map((tile, index) => (
-                        <GridListTile key={tile.img} cols={tile.cols || 1} title={tile.description}>
-                            <img src={tile.img} title={tile.description} alt={tile.title} onClick={(event) => {handleClick(event, tile.description)} } />
-                        </GridListTile> 
-                    ))}
-                </GridList>
 
+            <div className={classes.root} {...props}>  {/* props spread must be included for Grow transition to work */}
+                <Grow in={true} timeout={1400}>
+                    <GridList cellHeight={160} className={classes.gridList} cols={3}>
+                        {tileData.map((tile, index) => (
+                            <GridListTile key={tile.img} cols={tile.cols || 1} title={tile.description}>
+                                <img src={tile.img} title={tile.description} alt={tile.title} onClick={(event) => { handleClick(event, tile.description) }} />
+                            </GridListTile>
+                        ))}
+                    </GridList>
+                </Grow>
             </div>
             <p style={{ marginBottom: "1em" }}></p>
         </>
