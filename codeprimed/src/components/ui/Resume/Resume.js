ded34f109002f294d65f083bf5acdc8b7e56405f
Greg Totsline <jtotsline@gmail.com>
2020-08-05 19:55:20 -0400

    Use window.origin so protocol and host determined at runtime.

--- codeprimed/src/components/ui/Resume/Resume.js
+++ codeprimed/src/components/ui/Resume/Resume.js
@@ -13,7 +13,7 @@ import resumeStyles from './resumeStyles';
  * @param {*} props 
  */
 
-const locationHost = window.location.hostname;
+const origin = window.location.origin;
 
 const Resume = (props) => {
     const classes = resumeStyles();
@@ -48,7 +48,7 @@ const Resume = (props) => {
     }
 
     function download() {
-        window.open('https://' + locationHost + '/resume.pdf');
+        window.open(origin + '/resume.pdf');
     }
 
     return (
@@ -57,7 +57,7 @@ const Resume = (props) => {
         // 2. Want to package UI and web services backend into single Spring Boot app jar so all can run in embedded tomcat
         // and not have separate nodjs running. See article: https://medium.com/@mukundmadhav/build-and-deploy-react-app-with-spring-boot-and-mysql-6f888eb0c600 
         <Box className={classes.box} borderColor="primary.light" border={1}>
-            <Document file={{ url: "https://" + locationHost + "/resume.pdf" }} onLoadSuccess={onDocumentLoadSuccess} >
+            <Document file={{ url: origin + "/resume.pdf" }} onLoadSuccess={onDocumentLoadSuccess} >
                 <Page pageNumber={pageNumber} width={800} />
             </Document>
             <div>
